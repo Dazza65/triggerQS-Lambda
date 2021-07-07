57da8403029c9a7d88d434ac17a296a791ae395b
Darren Harris <dazza@me.com>
2021-07-07 10:18:43 +0800

    Updated the data set map to make it dynamic and to reference the modifed QuickSight job name.

--- triggerQS.js
+++ triggerQS.js
@@ -2,10 +2,7 @@ const AWSXRay = require('aws-xray-sdk-core');
 const { QuickSightClient, CreateIngestionCommand } = require("@aws-sdk/client-quicksight");
 const { v1: uuidv1 } = require('uuid')
 
-
 const qsClient = AWSXRay.captureAWSv3Client(new QuickSightClient({region: process.env.AWS_REGION}));
-
-const data_set_map = [{jobName: "jira-lttd", datasetID: "659fa345-4333-4fdf-8a99-25c935986c03"}];
 
 const triggerQSRefresh = async (account_id, dataSetId) => {
     try {
@@ -27,17 +24,16 @@ const triggerQSRefresh = async (account_id, dataSetId) => {
 }
 
 exports.handler = async (event, context) => {
-
     const account_id = event.account;
     const jobName = event.detail.jobName;
+    const data_set_map = [{jobName: "kpi-job", datasetID: `${account_id}-20210625-01`}];
     
     console.log(`JOB: ${jobName} with run ID ${event.detail.jobRunId} in ${event.detail.state} state.`);
 
     if( "SUCCEEDED" === event.detail.state ) {
-        const dataSetId = data_set_map.find(data_set => data_set.jobName === "jira-lttd").datasetID;
+        const dataSetId = data_set_map.find(data_set => data_set.jobName === "kpi-job").datasetID;
 
         const resp = await triggerQSRefresh(account_id, dataSetId);
         console.log(resp);
     }
-
 };
